9cf2c0bbf7038870fdbb4f1b6f83b9b9440d7dda
shiphan-pathan <shiphanpathan2001@gmail.com>
2026-01-11 20:03:40 +0530

    interview

---
+++ intervie.js
@@ -0,0 +1,34 @@
+// You are given an array of integers.
+// Your task is to count how many even numbers are present in the array and display the result in a React component.
+
+// Requirements
+
+// Use React functional components
+
+// Use JavaScript logic (DSA concept: iteration & condition checking)
+
+// Display:
+
+// The original array
+
+// The count of even numbers
+
+// Example
+
+// Input:
+// [1, 2, 3, 4, 5, 6]
+
+// Output:
+// Even Numbers Count: 3
+
+
+
+// Input
+
+// An array of integers
+// Example:
+// [3, 7, 2, 8, 10, 5]
+
+// Output
+
+// Highest Even Number: 10
