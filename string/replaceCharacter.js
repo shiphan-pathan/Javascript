6cb809b1b2e0ca2472f1880bfa2a1a8bd9f8e0d4
shiphan-pathan <shiphanpathan2001@gmail.com>
2026-01-18 09:52:13 +0530

    String DSA

---
+++ string/replaceCharacter.js
@@ -0,0 +1,12 @@
+let str = "javascript";
+let result = "";
+
+for (let i = 0; i < str.length; i++) {
+  if (i === 2 || i === 5 || i === 7) {
+    result += "X";   
+  } else {
+    result += str[i]; 
+  }
+}
+
+console.log(result);
